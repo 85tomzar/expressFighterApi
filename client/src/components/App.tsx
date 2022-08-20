5f3c78f98525f364496bdb41b4722d5c3713fe7e
Tommy Zarén <85tomzar@skola.boras.se>
2022-08-20 22:11:16 +0200

    fightercard component, fetch, layout, styling

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/app.css";
+import FighterCard from "./FighterCard";
+import Fighter from "../models/FighterModel";
 
 function App() {
+  const [fighters, setFighters] = useState([]);
+  const [selectedFighter, setSelectedFighter] = useState({} as Fighter);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/fighters")
+      .then((response) => response.json())
+      .then((data) => {
+        setFighters(data);
+      });
+  }, []);
+
+  const handleClick = (fighter: Fighter): void => {
+    setSelectedFighter(fighter);
+  };
+
+  const fighterCards = fighters.map((fighter: Fighter) => {
+    return (
+      <FighterCard key={fighter.id} onclick={handleClick} fighter={fighter} />
+    );
+  });
+
   return (
     <>
       <header>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/92/UFC_Logo.svg"
-          alt="UFC logo"
-          className="logo"
-        />
+        <div className="header-content">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/92/UFC_Logo.svg"
+            alt="UFC logo"
+            className="logo"
+          />
+          <div>
+            <i className="fa-solid fa-plus"></i> Add fighter
+          </div>
+        </div>
       </header>
-      <div>Hello world!</div>
+      <main>
+        <Container className="d-flex justify-content-center">
+          <div className="fighter-container">{fighterCards}</div>
+        </Container>
+      </main>
     </>
   );
 }
---
+++ client/src/components/FighterCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Fighter from "../models/FighterModel";
+import "./css/fightercard.css";
+
+interface Props {
+  fighter: Fighter;
+  key: string;
+  onclick: (arg: Fighter) => void;
+}
+
+export default function FighterCard(props: Props) {
+  return (
+    <Card
+      style={{ width: "10rem", cursor: "pointer" }}
+      onClick={(event) => props.onclick(props.fighter)}
+    >
+      <Card.Img
+        variant="top"
+        src={props.fighter.imgURL}
+        className="thumbnail"
+      />
+      <Card.Body>
+        <Card.Title>{props.fighter.name}</Card.Title>
+        {props.fighter.nickname !== "N/A" ? (
+          <Card.Subtitle className="mb-2 text-muted">
+            {props.fighter.nickname}
+          </Card.Subtitle>
+        ) : null}
+        <Card.Text>{props.fighter.record}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
