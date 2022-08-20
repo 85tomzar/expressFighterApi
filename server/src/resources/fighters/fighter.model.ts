8343cf396612dc2f0720cf2b5d2c1cc4c5cd0374
Tommy Zarén <85tomzar@skola.boras.se>
2022-08-20 09:52:23 +0200

    added fighter schema

--- server/src/resources/fighters/fighter.model.ts
+++ server/src/resources/fighters/fighter.model.ts
@@ -1,3 +1,5 @@
+import Joi from "joi";
+
 export interface Fighter {
   id: string;
   name: string;
@@ -10,3 +12,16 @@ export interface Fighter {
   reach: string;
   imgURL: string;
 }
+
+export const fighterSchema = Joi.object<Fighter, true>({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  nickname: Joi.string().required(),
+  division: Joi.string().required(),
+  record: Joi.string().required(),
+  age: Joi.string().required(),
+  height: Joi.string().required(),
+  weight: Joi.string().required(),
+  reach: Joi.string().required(),
+  imgURL: Joi.string().required(),
+});
