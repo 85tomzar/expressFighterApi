cdc868baf6caa6054fa6188ee2dc6494cf31dd1d
Tommy Zarén <85tomzar@skola.boras.se>
2022-08-20 20:03:54 +0200

    designed basic layout

---
+++ client/src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/app.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/92/UFC_Logo.svg"
+          alt="UFC logo"
+          className="logo"
+        />
+      </header>
+      <div>Hello world!</div>
+    </>
+  );
+}
+
+export default App;
